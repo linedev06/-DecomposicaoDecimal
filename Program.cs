f4c253eeb2126c3ea6ccf66ff751e7e8170aaffd
linedev06 <vitorafelismino@gmail.com>
2023-03-31 22:43:06 -0300

    exercicio até a metade

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Decomposição Decimal ---");
+
+Console.Write("Digite um número inteiro...");
+ int dezena = Convert.ToInt32(Console.ReadLine());    
+ int centena = Convert.ToInt32(Console.ReadLine()); 
+ int unidade = Convert.ToInt32(Console.ReadLine()); 
+ int numero = Convert.ToInt32(Console.ReadLine()); 
+ int restantes = Convert.ToInt32(Console.ReadLine()); 
